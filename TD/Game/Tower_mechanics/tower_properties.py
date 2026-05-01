ed4a03115d01dce441818a970a249d9a9aae142a
Chrmar0809 <Chrmar0809@highgateschool.org.uk>
2026-05-01 12:05:41 +0100

    the start

--- TD/Game/Tower_mechanics/tower_properties.py
+++ TD/Game/Tower_mechanics/tower_properties.py
@@ -1,1 +1,21 @@
-eeee
+class Enemy:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def take_damage(self, damage):
+        self.health -= damage
+        return f"{self.name} took {damage} damage"
+
+
+
+class Tower(object):
+    def __init__(self, tower_name, damage):
+        self.tower_name = tower_name
+        self.damage = damage
+
+    def shoot(self,enemy_name):
+        enemy_name.take_damage(self.damage)
+
+    def select_target(self):
+        target = None
